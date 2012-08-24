3fadffae8515c80f1355c55d79d499f996309502
louisluhao <louisluhao@gmail.com>
2012-08-24 17:45:12 -0400

    start programming

--- ACMWorldFinal/src/wf2012/b/CurvyLittleBottles.java
+++ ACMWorldFinal/src/wf2012/b/CurvyLittleBottles.java
@@ -3,43 +3,194 @@ package wf2012.b;
 import static java.lang.Math.PI;
 import static java.lang.Math.pow;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class CurvyLittleBottles {
 	
-	private static Map<Integer, Double> factorialChache = new HashMap<Integer, Double>(); 
-	
-	public static double combination(int n, int m){
-		if(m == 0) return 1;
-		return factorial(n) / factorial(m);
+	public static void main(String[] args){
+		Polynominal case1 = new Polynominal();
+		case1.add(new PolyTerm(4, 0));
+		case1.add(new PolyTerm(-0.25, 1));
+		Polynominal circleArea = new Polynominal();
+		circleArea = Polynominal.multiple(case1, case1);
+		circleArea.integral();
+		System.out.println(circleArea.getValue(0.515)*PI);
+		
+		Polynominal case2 = new Polynominal();
+		case2.add(new PolyTerm(1.7841241161782, 0));
+		Polynominal circleArea2 = new Polynominal();
+		circleArea2 = Polynominal.multiple(case2, case2);
+		circleArea2.integral();
+		System.out.println(circleArea2.getValue(10)*PI - circleArea2.getValue(-5)*PI);		
 	}
 	
-	private static double factorial (int x){
-		if(factorialChache.get(x) != null){
-			return factorialChache.get(x);
+	
+	
+	
+	//polynominal and polyterm
+
+
+public static class Polynominal {
+	
+	Map<Double, PolyTerm> polyMap = new HashMap<Double, PolyTerm>();
+	
+	
+	public void add(PolyTerm p){
+		if(polyMap.containsKey(p.exponent)){
+			polyMap.put(p.exponent, polyMap.get(p.exponent).add(p));
 		}
-		double result = 1;
-		for(int i = 2; i <= x ; i++){
-			result = result * i;
+		else{
+			polyMap.put(p.exponent, p);
 		}
-		factorialChache.put(x, result);
+	}
+	
+	public PolyTerm getPolyTermByExponent(double exp){
+		return polyMap.get(exp);
+	}
+	
+	public Collection<PolyTerm> getPolyTerms(){
+		return polyMap.values();
+	}
+	
+	public double getValue(double x){
+		double ans = 0;
+		for(PolyTerm term : polyMap.values()){
+			ans += term.getValue(x);
+		}
+		return ans;
+	}
+	
+//	public Polynominal toPower(int power){
+//		Polynominal rePoly = new Polynominal();
+//	}
+	
+	public static Polynominal multiple(Polynominal a, Polynominal b){
+		Polynominal repoly = new Polynominal();
+		for(PolyTerm aterm : a.getPolyTerms())
+			for(PolyTerm bterm : b.getPolyTerms()){
+				repoly.add(aterm.multiple(bterm));
+			}
+		return repoly;
+	}
+	
+	public void integral(){
+		Map<Double, PolyTerm> old = new HashMap<Double, PolyTerm>(polyMap);
+		polyMap.clear();
+		for(PolyTerm poly : old.values()){
+			this.add(poly.Integral());
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((polyMap == null) ? 0 : polyMap.hashCode());
 		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Polynominal other = (Polynominal) obj;
+		if (polyMap == null) {
+			if (other.polyMap != null)
+				return false;
+		} else if (!polyMap.equals(other.polyMap))
+			return false;
+		return true;
+	}
 	
-	private static List<Double> getEmptyCoefficients(int power){
-		List<Double> coefficients = new ArrayList<Double>();
-		for(int i = 0; i < power; i++){
-			coefficients.add(0.0);
+	
+}
+	
+public static class PolyTerm {
+	double coefficient;
+	double exponent;
+
+	public PolyTerm(double coefficient, double exponent) {
+		this.coefficient = coefficient;
+		this.exponent = exponent;
+	}
+
+	public PolyTerm add(PolyTerm other) {
+		if (other.exponent != this.exponent) {
+			return null;
+		} else {
+			return new PolyTerm(this.coefficient + other.coefficient,
+					this.exponent);
 		}
-		return coefficients;
 	}
-	
-	
-	
-	public static void main(String[] args){
-		
+
+	public PolyTerm sub(PolyTerm other) {
+		if (other.exponent != this.exponent) {
+			return null;
+		} else {
+			return new PolyTerm(this.coefficient - other.coefficient,
+					this.exponent);
+		}
 	}
+
+	public PolyTerm multiple(PolyTerm other) {
+		return new PolyTerm(this.coefficient * other.coefficient, this.exponent
+				+ other.exponent);
+	}
+
+	public double getValue(double x) {
+		return this.coefficient * pow(x, exponent);
+	}
+
+	public static boolean isSameExponent(PolyTerm a, PolyTerm b) {
+		return a.exponent == b.exponent;
+	}
+
+	public PolyTerm Integral() {
+		return new PolyTerm(this.coefficient / (this.exponent + 1),
+				exponent + 1);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(coefficient);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(exponent);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PolyTerm other = (PolyTerm) obj;
+		if (Double.doubleToLongBits(coefficient) != Double
+				.doubleToLongBits(other.coefficient))
+			return false;
+		if (Double.doubleToLongBits(exponent) != Double
+				.doubleToLongBits(other.exponent))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PolyTerm [coefficient=" + coefficient + ", exponent="
+				+ exponent + "]";
+	}
+
+}
 }
